30eea55d43e610ab3b00dfcb4b0cc74fffe1f241
milina <>
2025-06-16 20:01:26 +0700

    chore: test 3

--- GithubUsers/Resources/Constants/Constants.swift
+++ GithubUsers/Resources/Constants/Constants.swift
@@ -24,13 +24,7 @@ extension Constants {
     }
 }
 
-enum DummyTestConstants {
-    static let testValue = "TestValue"
-    static let testNumber = 42
-    static let testFlag = true
-}
-
-enum DummyTestConstants3 {
+enum DummyTestConstants4 {
     static let testValue = "TestValue"
     static let testNumber = 42
     static let testFlag = true
